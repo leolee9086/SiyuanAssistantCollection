0a44ad786a16cfdf2054c52144ec43f7350df169
leolee9086 <464803712@qq.com>
2023-09-19 19:32:29 +0800

    增加动作表:打开最近块

--- installed/actionList/open.js
+++ installed/actionList/open.js
@@ -5,10 +5,9 @@ export default (context) => {
     return recentDocs.map(
         doc => {
             return {
-                label: '打开' + doc.title,
+                label: '打开:' + doc.title,
                 hints: doc.title,
                 hintAction:async (context)=>{
-                    context.token?context.token.delete():null
                     await (new BlockHandler(doc.rootID,kernelApi)).open()
                     window.siyuan.menus.menu.remove()
                 }
