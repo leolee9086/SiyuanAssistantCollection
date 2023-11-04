3f21d183977ab3b2025080f77a7c7f0b07070f83
leolee9086 <464803712@qq.com>
2023-11-04 10:40:42 +0800

    清理无用代码

--- source/throne/shellFactory/drivers/searcher.js
+++ source/throne/shellFactory/drivers/searcher.js
@@ -2,7 +2,6 @@ import { embeddingText } from "../../../utils/textProcessor.js"
 import { plugin } from "../../index.js"
 import { searchBlock } from "./blockSearcher.js"
 import { logger } from "../../../logger/index.js"
-import { searchBaidu } from "./webSearcher.js"
 import { jieba } from '../../../utils/tokenizer.js'
 import { 组合函数 } from "../../../baseStructors/functionTools.js"
 export async function searchRef(message) {
@@ -21,32 +20,24 @@ export async function searchRef(message) {
             }
         } catch (e) {
             logger.aishellerror(refs)
-
         }
         logger.aishelllog(refs)
         try {
-
             if (plugin.configurer.get('自动搜索设置', '搜索结果中包含网络搜索结果').$value) {
                 let text = message.content || (message.meta && message.meta.content)
                 let search = plugin.搜索管理器.get('webseacher', 'baidu')
                 let fArray = []
                 fArray.push(async () => { return await search(text) })
-                //            let result1 = await searchBaidu(text);
-                //            webSearcherResults.push(result1);
                 let tokens = jieba.tokenize(text, "search")
                 for (let token of tokens) {
                     if (token.word.length > 2) {
-                        //  let result = await searchBaidu(token.word);
                         fArray.push(async () => { return await search(token.word) })
-                        // webSearcherResults.push(result);
                     }
                 }
                 refs += ('\n' + (await 组合函数(fArray)()).join('\n'))
-                //refs += '\n' + webSearcherResults.join('\n')
             }
         } catch (e) {
             logger.aishellerror(refs)
-
         }
         logger.aishelllog(refs)
         return refs
