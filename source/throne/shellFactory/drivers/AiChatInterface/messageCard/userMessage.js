d987163e92754d144e1facbd470bc76810ad94e0
leolee9086 <464803712@qq.com>
2023-11-11 21:55:02 +0800

    重构AI聊天界面

--- source/throne/shellFactory/drivers/AiChatInterface/messageCard/userMessage.js
+++ source/throne/shellFactory/drivers/AiChatInterface/messageCard/userMessage.js
@@ -1,8 +1,12 @@
-import { createElementWithTagname } from "../../../../../UI/builders/index.js";
-export const 创建用户消息卡片=(message)=>{
-    let { content, id } = message
-    const userMessage = createElementWithTagname("div", ["user-message", "fn__flex"], `<div class="fn__flex"><strong>User:</strong> ${content}</div>`);
-    userMessage.appendChild(createElementWithTagname('div', ["fn__space", "fn__flex-1"], ``))
-    userMessage.setAttribute('data-message-id', id)
-    return userMessage
+import { createElementWithTagname,string2DOM } from "../../../../../UI/builders/index.js";
+
+export const 创建用户消息卡片 = (message) => {
+    let { content, id } = message;
+    const userMessage = string2DOM(`
+        <div class="user-message fn__flex" data-message-id="${id}">
+            <div class="fn__flex"><strong>User:</strong> ${content}</div>
+            <div class="fn__space fn__flex-1"></div>
+        </div>
+    `);
+    return userMessage;
 }
