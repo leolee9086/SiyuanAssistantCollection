183f19fd4135019a0b8de83118fcb44509008910
leolee9086 <464803712@qq.com>
2023-11-12 10:36:06 +0800

    重构代码

--- source/throne/shellFactory/drivers/AiChatInterface/messageCard/userMessage.js
+++ source/throne/shellFactory/drivers/AiChatInterface/messageCard/userMessage.js
@@ -1,16 +1,16 @@
 import { string2DOM ,createElementWithTagname} from "../../../../../UI/builders/index.js";
 
-export const 创建用户消息卡片 = (message) => {
+export const 创建用户消息卡片 = (message,doll) => {
     let { content, id } = message;
     const userMessage = string2DOM(`
         <div class="user-message fn__flex" data-message-id="${id}">
             <div class="fn__flex"><strong>User:</strong> ${content}</div>
             <div class="fn__space fn__flex-1"></div>
+            <span ><svg class="b3-menu__icon" data-action="trash" style=""><use xlink:href="#iconTrashcan"></use></svg></span>
+
         </div>
     `);
-    let trashButton = createElementWithTagname('span', [], `<svg class="b3-menu__icon " style=""><use xlink:href="#iconTrashcan"></use></svg>`)
-    trashButton.addEventListener('click', () => { this.doll.emit('human-forced-forget-to', id) })
-    userMessage.appendChild(trashButton)
-
+    let trashButton = userMessage.querySelector('[data-action="trash"]')
+    trashButton.addEventListener('click', () => { doll.emit('human-forced-forget-to', id) })
     return userMessage;
 }
