ffe3840cd88eabd739b3c1d0eac5285789421688
leolee9086 <464803712@qq.com>
2023-11-25 18:24:47 +0800

    清理代码

--- source/utils/workerHandler.js
+++ source/utils/workerHandler.js
@@ -74,7 +74,6 @@ function 计算cpu核心数量(){
 function 正规化URL(原始URL) {
   // 创建一个URL对象
   let url = new URL(原始URL,location.href);
-
   // 返回正规化的URL，并替换连续的//
   return url.href.replace(/([^:])\/\/+/g, '$1/');
 }
