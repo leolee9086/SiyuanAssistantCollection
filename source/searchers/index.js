d75aca8b5ed1c121e7174c3e112265d35df57357
leolee9086 <464803712@qq.com>
2023-11-26 21:28:56 +0800

    解耦数据搜索实现

--- source/searchers/index.js
+++ source/searchers/index.js
@@ -3,6 +3,7 @@ import { 组合函数 } from "../baseStructors/functionTools.js"
 import { 提取文本向量 } from "../utils/textProcessor.js"
 import { searchBaidu, searchWeibo } from "./websearchers/webSearcher.js"
 import { seachBlockWithVector } from "./blocksearchers/vectorSearcher.js"
+import { seachBlockWithText } from "./blocksearchers/simpleTextSearcher.js"
 export async function 以文本查找最相近文档(textContent, count, 查询方法, 是否返回原始结果, 前置过滤函数, 后置过滤函数) {
     let embedding = await 提取文本向量(textContent)
     let vectors = plugin.块数据集.以向量搜索数据('vector', embedding, count, 查询方法, 是否返回原始结果, 前置过滤函数, 后置过滤函数)
@@ -42,5 +43,7 @@ plugin.searchers = {
 plugin.searchers.set('baidu', { search: searchBaidu },)
 plugin.searchers.set('weibo', { search: searchWeibo },)
 plugin.searchers.set('vector',{search:seachBlockWithVector},'blockSearcher')
+plugin.searchers.set('text',{search:seachBlockWithText},'blockSearcher')
+
 export const set = (...args) => { plugin.searchers.set(...args) }
 export const get = (...args) => { return plugin.searchers.get(...args) }
--- source/throne/shellFactory/drivers/blockSearcher.js
+++ source/throne/shellFactory/drivers/blockSearcher.js
@@ -3,43 +3,8 @@ import kernelApi from '../../../polyfills/kernelApi.js'
 import { plugin } from '../../../asyncModules.js'
 import { logger } from '../../../logger/index.js'
 export const seachBlockWithVector= plugin.searchers.get('blockSearcher', 'vector')
-export const seachBlockWithText = async (text) => {
-    let tokens = jieba.tokenize(text, "search")
-    let query = ""
-    for (let token of tokens) {
-        if (token.word.length >= 2) {
-            query += ` OR "${token.word}"`
-        }
-    }
-    query = query.replace("OR").trim()
-    let data = await kernelApi.fullTextSearchBlock({
-        "query": query,
-        "method": 1,
-        "types": { "document": true, "heading": true, "list": false, "listItem": false, "codeBlock": true, "htmlBlock": true, "mathBlock": true, "table": true, "blockquote": true, "superBlock": true, "paragraph": true, "embedBlock": false },
-        "paths": [],
-        "groupBy": 0,
-        "orderBy": 0,
-        "page": 1
+export const seachBlockWithText= plugin.searchers.get('blockSearcher', 'text')
 
-    })
-    if (data && data.blocks[0]) {
-        // 计算每个块与查询文本的共同词素数量
-        data.blocks.forEach(block => {
-            let blockTokens = jieba.tokenize(block.content, "search")
-            block.commonTokensCount = blockTokens.filter(token => tokens.includes(token)).length
-            if (plugin.configurer.get('聊天工具设置', '发送参考时文档和标题块发送全部内容').$value && block.type === 'd' || block.type === 'h') {
-                let content = plugin.lute.BlockDOM2Text(kernelApi.getDoc.sync({ id: block.id, size: 102400 }).content)
-                block.content = content
-                return block
-            }
-        })
-        // 根据共同词素数量对块进行排序
-        data.blocks.sort((a, b) => b.commonTokensCount - a.commonTokensCount)
-        // 返回共同词素数量最多的十个块
-        return data.blocks.slice(0, plugin.configurer.get('聊天工具设置', '默认参考数量').$value || 10)
-    }
-    else return []
-}
 export const searchBlock = async (message, vector) => {
     let blocks1 = []
     try {
