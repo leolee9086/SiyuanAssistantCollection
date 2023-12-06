76b2a581c6ba7a9772c5ddc7861115fde4d9180c
leolee9086 <464803712@qq.com>
2023-12-06 17:33:20 +0800

    重构代码

--- source/UI/runtime.js
+++ source/UI/runtime.js
@@ -2,7 +2,7 @@ import { 获取光标所在位置 } from "../utils/rangeProcessor.js";
 import { 使用结巴拆分元素 } from "../utils/tokenizer.js";
 import { logger } from "../logger/index.js";
 import { 智能防抖 } from "../utils/functionTools.js"
-import { 根据上下文获取动作表 } from "./tokenMenu.js";
+import { 根据上下文获取动作表 } from "../actionList/getter.js";
 import { kernelApi } from "../asyncModules.js";
 export {
     获取光标所在位置,
