a7912382026a0745834cc688a0e65a6a7a52db02
leolee9086 <464803712@qq.com>
2023-12-15 17:51:23 +0800

    清理无用代码

--- source/Managers/routerManager/index.js
+++ source/Managers/routerManager/index.js
@@ -2,31 +2,6 @@ import _Router from "./router.js"
 import { sac } from "../../asyncModules.js"
 export const 根路由 = new _Router()
 export const Router = _Router
-export const 注册基本路由 = (路由名称, 基本路由, ctx构建器) => {
-    // 检查基本路由和ctx构建器是否提供
-    if (!基本路由 || !ctx构建器) {
-        throw new Error('必须提供基本路由和ctx构建器');
-    }
-
-    // 检查基本路由是否是函数
-    if (typeof 基本路由 !== 'function') {
-        throw new Error('基本路由必须是函数');
-    }
-    // 检查ctx构建器是否是函数
-    if (typeof ctx构建器 !== 'function') {
-        throw new Error('ctx构建器必须是函数');
-    }
-    // 创建中间件函数
-    const 中间件 = async (ctx, next) => {
-        // 使用ctx构建器构建上下文
-        const 新ctx = ctx构建器(ctx);
-
-        // 调用基本路由处理请求
-        await 基本路由(新ctx, next);
-    };
-    // 将中间件添加到路由
-    sac.根路由.all(路由名称, 中间件);
-};
 export const internalFetch = async (path, options = {}) => {
     // 创建上下文对象
     const ctx = {
