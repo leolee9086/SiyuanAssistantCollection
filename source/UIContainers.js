a4c7fcabab261f178178c952648699472f5823e3
leolee9086 <464803712@qq.com>
2023-12-30 17:01:13 +0800

    创建集市管理器按钮

--- source/UIContainers.js
+++ source/UIContainers.js
@@ -1,14 +1,23 @@
 //这里的require注入了plugin对象
 const plugin = require('plugin');
 const clientApi = require('clientApi');
-let topBarButton = plugin.addTopBar(
+let topBarAiButton = plugin.addTopBar(
   {
     icon: 'iconSparkles',
     title: '打开对话框,右键打开设置',
     position: 'right',
   }
 )
-plugin.statusMonitor.set('UI', 'topBarButton', topBarButton)
+plugin.statusMonitor.set('UI', 'topBarAiButton', topBarAiButton)
+let topBarBerryButton = plugin.addTopBar(
+  {
+    icon: 'iconBerry',
+    title: '打开对话框,右键打开设置',
+    position: 'right',
+  }
+)
+plugin.statusMonitor.set('UI', 'topBarBerryButton', topBarBerryButton)
+
 function 创建Tab容器() {
   const DOCK_TYPE = 'SAC_Tab'
   plugin.TabContainer = plugin.addTab({
