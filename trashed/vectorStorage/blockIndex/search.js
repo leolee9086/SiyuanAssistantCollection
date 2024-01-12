c6e9351fba2781037a865c21a294d64f8c413fb9
leolee9086 <464803712@qq.com>
2024-01-12 23:51:25 +0800

    清理代码

--- trashed/vectorStorage/blockIndex/search.js
+++
@@ -1,5 +0,0 @@
-import { logger } from "../../../source/logger/index.js";
-import { plugin } from "../../../source/asyncModules.js";
-export let searchWithVector = async(...args)=>{
-    return await plugin.块数据集.以向量搜索数据(...args)
-}
