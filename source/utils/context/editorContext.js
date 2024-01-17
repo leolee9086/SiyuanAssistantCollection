21d983108340ce1dcfbd81ec8251e1ba0e3bf3dd
leolee9086 <464803712@qq.com>
2024-01-17 20:10:03 +0800

    重构代码-增强编辑器上下文实现

--- source/utils/context/editorContext.js
+++ source/utils/context/editorContext.js
@@ -8,6 +8,12 @@ export function 创建编辑器上下文() {
     return null;
   }
   let 分词结果数组 = 使用结巴拆分元素(editableElement);
+  const 当前光标所在分词结果 = 获取当前光标所在分词结果(编辑器上下文.tokens, 编辑器上下文.position);
+  if (!当前光标所在分词结果) {
+    return;
+  }
+  编辑器上下文.currentToken = 当前光标所在分词结果;
+
   return {
     position: pos,
     text: editableElement.innerText,
