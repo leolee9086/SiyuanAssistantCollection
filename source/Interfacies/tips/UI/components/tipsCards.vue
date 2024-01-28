58c919bf1d30fe735b7dfd1c21150e84144c21a5
leolee9086 <464803712@qq.com>
2024-01-29 03:50:08 +0800

    清理代码

--- source/Interfacies/tips/UI/components/tipsCards.vue
+++ source/Interfacies/tips/UI/components/tipsCards.vue
@@ -153,28 +153,22 @@ function 高亮目标块(item) {
     }
 }
 function 切换钉住状态(item){
-    console.log(item.pined)
     if (item.pined) {
     item.scores.pined=Infinity
     let tips = sac.statusMonitor.get('tips', 'current').$value || [];
-
     // 找到item在tips中的位置
     let index = tips.findIndex(tip => tip === item);
-
         // 找到最后一个被pin的元素的位置
         let lastPinnedIndex = tips.lastIndexOf(tip => tip.pined);
-
         // 如果没有其他被pin的元素，那么item应该被移动到数组的开始
         if (lastPinnedIndex === -1) {
             lastPinnedIndex = 0;
         }
-
         // 移动item到最后一个被pin的元素的后面
         tips.splice(index, 1);
         tips.splice(lastPinnedIndex + 1, 0, item);
     }else{
         item.scores.pined=0
-
     }
 }
 
--- trashed/configBuilder.js
+++ trashed/configBuilder.js
@@ -64,20 +64,3 @@ export function addSetting() {
 
 }
 
-const 创建悬浮球 = () => {
-    // 创建一个新的div元素，这将是我们的悬浮球
-    let 悬浮球 = document.createElement('div');
-
-    // 设置悬浮球的样式
-    悬浮球.style.position = 'fixed';  // 固定位置
-    悬浮球.style.bottom = '20px';     // 距离底部20px
-    悬浮球.style.right = '20px';      // 距离右边20px
-    悬浮球.style.width = '50px';      // 宽度50px
-    悬浮球.style.height = '50px';     // 高度50px
-    悬浮球.style.borderRadius = '50%'; // 边框半径50%，使其成为一个圆形
-    悬浮球.style.backgroundColor = 'red'; // 背景色红色
-
-    // 将悬浮球添加到页面中
-    document.body.appendChild(悬浮球);
-    return 悬浮球
-};
