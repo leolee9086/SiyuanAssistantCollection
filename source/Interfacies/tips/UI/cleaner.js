1381683e3ef08353f508af449cbb78fac4e24e19
leolee9086 <464803712@qq.com>
2024-01-30 18:25:07 +0800

    重构代码

--- source/Interfacies/tips/UI/cleaner.js
+++ source/Interfacies/tips/UI/cleaner.js
@@ -1,13 +1,13 @@
 import { MinHeap as 最小堆 } from "../../../utils/Array/minHeap.js";
 import { sac } from "../runtime.js";
 // 限制待添加数组的长度，只保留最新的10个元素，同时保持原有顺序
-export async function 限制待添加数组长度(待添加数组,num) {
-    if (待添加数组.length > (num || 1000)) {
-        移除每个维度最低分的项目(待添加数组);
+export async function 限制待添加数组长度(tips数组,num) {
+    if (tips数组.length > (num || 1000)) {
+        移除每个维度最低分的项目(tips数组);
     }
-    if (待添加数组.length > (num || 1000)) {
+    if (tips数组.length > (num || 1000)) {
         // 根据time属性创建一个映射，然后根据time降序排序
-        const sortedByTime = 待添加数组
+        const sortedByTime = tips数组
             .map((item, index) => ({ index, time: item.time, pined: item.pined }))
             .sort((a, b) => {
                 if (a.pined !== b.pined) {
@@ -18,63 +18,58 @@ export async function 限制待添加数组长度(待添加数组,num) {
             })
             .slice(0, 10) // 选择最新的100个元素
             .map(item => item.index); // 转换回原数组的索引
-
-
         // 创建一个新数组，包含原数组中最新的20个元素，保持原有顺序
-        待添加数组 = sortedByTime
+        tips数组 = sortedByTime
             .sort((a, b) => a - b) // 按原索引升序排序以保持原顺序
-            .map(index => 待添加数组[index]);
+            .map(index => tips数组[index]);
     }
-    sac.statusMonitor.set('tips', 'current', 待添加数组);
+    sac.statusMonitor.set('tips', 'current', tips数组);
 }
-
-
-
-const 移除每个维度最低分的项目 = (待添加数组) => {
+const 移除每个维度最低分的项目 = (tips数组) => {
     // 找出所有存在的维度
-    const allDimensions = new Set();
-    for (const item of 待添加数组) {
-        if (item.scores) {
-            for (const dimension of Object.keys(item.scores)) {
-                allDimensions.add(dimension);
+    const 所有评分维度 = new Set();
+    for (const tips条目 of tips数组) {
+        if (tips条目.scores) {
+            for (const 评分维度 of Object.keys(tips条目.scores)) {
+                所有评分维度.add(评分维度);
             }
         }
     }
     // 对于每个维度，使用最小堆找出得分最低的项目并移除它
-    for (const dimension of allDimensions) {
+    for (const dimension of 所有评分维度) {
         const heap = new 最小堆((a, b) => a.scores[dimension] - b.scores[dimension]);
-        for (const item of 待添加数组) {
+        for (const item of tips数组) {
             if (item.scores && item.scores[dimension] !== undefined) {
                 heap.push(item);
             }
         }
         // 如果堆中有元素，则移除得分最低的项目
         if (heap.size() > 1) {
-            const lowestScoreItem = heap.pop();
-            const indexToRemove = 待添加数组.findIndex(item => item === lowestScoreItem);
-            if (indexToRemove !== -1) {
-                待添加数组.splice(indexToRemove, 1);
+            const 最低分项目 = heap.pop();
+            const 待移除索引 = tips数组.findIndex(item => item === 最低分项目);
+            if (待移除索引 !== -1) {
+                tips数组.splice(待移除索引, 1);
             }
         }
     }
 };
 // 去重待添加数组中的元素，并去除description短于两个字符的元素
-export function 去重待添加数组(待添加数组) {
-    待添加数组 = 待添加数组.reduce((unique, item) => {
-        if (item.description && item.description.length < 2) {
-            return unique; // 如果description短于两个字符，则不添加到数组中
+export function 去重待添加数组(tips数组) {
+    tips数组 = tips数组.reduce((去重结果, tips条目) => {
+        if (tips条目.description && tips条目.description.length < 2) {
+            return 去重结果; // 如果description短于两个字符，则不添加到数组中
         }
-        let isDuplicate = unique.some(
+        let 是否重复 = 去重结果.some(
             //同源且同描述的tips会被视为重复而清除
-            u => u.source === item.source
+            u => u.source === tips条目.source
                 &&
-                u.description === item.description && !item.pined
+                u.description === tips条目.description && !tips条目.pined
         );
-        if (!isDuplicate && !item.deleted) {
-            unique.push(item);
+        if (!是否重复 && !tips条目.deleted) {
+            去重结果.push(tips条目);
         }
-        return unique;
+        return 去重结果;
     }, []);
-    sac.statusMonitor.set('tips', 'current', 待添加数组);
+    sac.statusMonitor.set('tips', 'current', tips数组);
 }
 
